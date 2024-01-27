1d162417fd5ed456b3d5e923f5a5269667561295
amoljagtap17 <amoljagtap17@gmail.com>
2024-01-27 22:36:39 +0530

    message-passing-projects: update code in locations api

--- modules/locations-api/wsgi.py
+++ modules/locations-api/wsgi.py
@@ -5,5 +5,5 @@ from app import create_app, run_consumer
 app = create_app(os.getenv("FLASK_ENV") or "test")
 
 if __name__ == "__main__":
-    run_consumer()
     app.run(debug=True)
+    run_consumer()
